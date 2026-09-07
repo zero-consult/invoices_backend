6b264b284d87971bab07c7b0181046194936c1a0
Mathias Ver Elst <mathias.ver.elst@gmail.com>
2026-09-07 19:06:54 +0200

    Passing JWT token to timesheet API

--- src/main/java/org/zero_consult/invoices_backend/services/TimesheetApiService.java
+++ src/main/java/org/zero_consult/invoices_backend/services/TimesheetApiService.java
@@ -1,17 +1,26 @@
 package org.zero_consult.invoices_backend.services;
 
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.WebApplicationContext;
+import org.zero_consult.idl.client.ApiClient;
+import org.zero_consult.idl.client.Configuration;
 import org.zero_consult.idl.client.api.TimesheetApi;
 import org.zero_consult.invoices_backend.configuration.CustomProperties;
+import org.zero_consult.invoices_backend.security.JwtUtil;
+import org.zero_consult.invoices_backend.utils.RequestHelper;
 
 @Service
+@Scope(value = WebApplicationContext.SCOPE_REQUEST, proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class TimesheetApiService {
 
-    private final CustomProperties customProperties;
     private final TimesheetApi timesheetsApi;
 
-    public TimesheetApiService(CustomProperties customProperties) {
-        this.customProperties = customProperties;
+    public TimesheetApiService(CustomProperties customProperties, JwtUtil jwtUtil) {
+        String jwtToken = jwtUtil.resolveToken(RequestHelper.getCurrentHttpRequest());
+        ApiClient apiClient = Configuration.getDefaultApiClient();
+        apiClient.setAccessToken(jwtToken);
         this.timesheetsApi = new TimesheetApi();
         this.timesheetsApi.setCustomBaseUrl(customProperties.getTimesheetBackendHost());
     }
